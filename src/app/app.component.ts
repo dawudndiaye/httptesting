b0712bac0223abdcbd2f56282341e4e68ede15cb
Dawud <ndiaye.dawud@gmail.com>
2022-12-24 17:03:44 +0000

    bing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,55 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { map } from 'rxjs/operators';
+import { Product } from './model/products';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'httppractice';
+export class AppComponent{
+  title = 'AngularHttpRequest';
+  allProducts: Product[]
+
+  constructor(private http: HttpClient) {
+
+  }
+
+  ngOnInit() {
+    this.fetchProducts();
+  }
+
+  onProductsFetch() {
+    this.fetchProducts();
+  }
+
+  onProductCreate(products: {pName: string, desc: string, price: string}){
+   console.log(products);
+   const headers = new HttpHeaders({'MyHeader': 'myself'})
+   this.http.post('https://httppractice-fbad7-default-rtdb.firebaseio.com/products.json', products, {headers: headers})
+   .subscribe((res) => {
+     console.log(res)
+   });
+  }
+  private fetchProducts() {
+    this.http.get<{[key: string]: Product}>('https://httppractice-fbad7-default-rtdb.firebaseio.com/products.json')
+    .pipe(map((res) => {
+      const products = [];
+      for(const key in res){
+        if(res.hasOwnProperty(key)){
+          products.push({...res[key], id: key})
+        }
+       
+      }
+      return products;
+    }))
+    .subscribe((products) => {
+      console.log(products)
+      this.allProducts = products;
+    })
+  }
 }
+
+
